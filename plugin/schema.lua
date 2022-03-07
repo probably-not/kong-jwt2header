0e80b43d10c1e4ed3ad426ec102c2245b2a581de
Coby Benveniste <coby@base.la>
2022-03-07 10:22:19 +0200

    Add formatter and format all lua code

--- plugin/schema.lua
+++ plugin/schema.lua
@@ -1,29 +1,31 @@
 local typedefs = require "kong.db.schema.typedefs"
 
-
 return {
-  name = "jwt-to-header",
-  fields = {
-    {
-      route = typedefs.no_route,
-    },
-    {
-      service = typedefs.no_service,
-    },
-    {
-      consumer = typedefs.no_consumer,
-    },
-    {
-      protocols = typedefs.protocols_http,
-    },
-    {
-      config = {
-        type = "record",
-        fields = {
-            {  strip_claims = { type     = "string", required = true, default  = "false" }, },
-            {  token_required = { type     = "string", required = true, default  = "true" }, },          
-        },
-      },
-    },
-  },
+    name = "jwt-to-header",
+    fields = {{
+        route = typedefs.no_route
+    }, {
+        service = typedefs.no_service
+    }, {
+        consumer = typedefs.no_consumer
+    }, {
+        protocols = typedefs.protocols_http
+    }, {
+        config = {
+            type = "record",
+            fields = {{
+                strip_claims = {
+                    type = "string",
+                    required = true,
+                    default = "false"
+                }
+            }, {
+                token_required = {
+                    type = "string",
+                    required = true,
+                    default = "true"
+                }
+            }}
+        }
+    }}
 }
